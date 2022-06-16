5b12336574ec93ae538bea5bb859d67ac140c8b7
culeforlife10 <57781340+culeforlife10@users.noreply.github.com>
2022-06-16 20:45:47 +0530

    Update chiller.py

--- chiller.py
+++ chiller.py
@@ -117,9 +117,9 @@ def UI():
     end_date = st.date_input('Upto what date would you like to train the model on ?',datetime.strptime('03/01/2022',"%m/%d/%Y"),
     min_value=datetime.strptime('06/30/2021',"%m/%d/%Y"),max_value= datetime.strptime('03/31/2022',"%m/%d/%Y"))
     col1=st.columns(3)
-    if col1.button('Predict'):
+    if st.button('Predict'):
         data=predict(end_date)
-        col1.write(data)
+        print(data)
         graph = Image.open('testvspred.png')
         st.image(graph)
     
