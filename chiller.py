3293b21aeb222e65d9301065bb52a0ed89b3927d
culeforlife10 <57781340+culeforlife10@users.noreply.github.com>
2022-06-16 20:03:44 +0530

    Update chiller.py

--- chiller.py
+++ chiller.py
@@ -118,9 +118,9 @@ def UI():
     min_value=datetime.strptime('06/30/2021',"%m/%d/%Y"),max_value= datetime.strptime('03/31/2022',"%m/%d/%Y"))
 
     if st.button('Predict'):
-        col1=st.columns(1)
+        col1=st.columns(5)
         with col1:
-            st.dataframe(predict(end_date),1000,1000)
+            col1.write(predict(end_date),1000,1000)
         graph = Image.open('testvspred.png')
         st.image(graph)
     
